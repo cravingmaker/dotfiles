07d687a5b44803100d4b39eeaa71f35f168ebd2f
Craving Maker <cravingmaker.id@gmail.com>
2024-12-18 11:10:49 +0700

    chore(package.json): add @ianvs/prettier-plugin-sort-imports dev-dependency

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,19 @@
 export default {
 	useTabs: true,
 	printWidth: 120,
-	plugins: ["prettier-plugin-packagejson", "prettier-plugin-sh"],
+	plugins: ["prettier-plugin-packagejson", "prettier-plugin-sh", "@ianvs/prettier-plugin-sort-imports"],
+	importOrder: [
+		"<TYPES>",
+		"<TYPES>^[.]",
+		"",
+		"<BUILTIN_MODULES>",
+		"",
+		"^react$",
+		"^react-dom$",
+		"",
+		"<THIRD_PARTY_MODULES>",
+		"",
+		"^(?!.*[.]css$)[./].*$",
+		".css$",
+	],
 };
