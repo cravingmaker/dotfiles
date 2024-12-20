8d8859ffc17dd8689622c42427b2c6048dce64cb
Craving Maker <cravingmaker.id@gmail.com>
2024-12-20 11:00:19 +0700

    chore(eslint): combine and optimize ESLint configurations
    - Merged separate ESLint configurations for ECMAScript, React, and TypeScript into a single file.

--- src/configs/eslint/eslint.config.mjs
+++ src/configs/eslint/eslint.config.mjs
@@ -2,19 +2,78 @@ import commentsPlugin from "@eslint-community/eslint-plugin-eslint-comments/conf
 import eslint from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
 import importPlugin from "eslint-plugin-import";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
-	// ESLint
+	/* Base ECMAScript configs */
 	eslint.configs.recommended,
 	commentsPlugin.recommended,
 	importPlugin.flatConfigs.recommended,
 
+	/* If uses React */
+	{
+		files: ["**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}"],
+		plugins: {
+			react: reactPlugin,
+			"react-hooks": reactHooksPlugin,
+			"jsx-a11y": jsxA11yPlugin,
+		},
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			globals: {
+				...globals.browser,
+			},
+		},
+		rules: {
+			...reactHooksPlugin.configs.recommended.rules,
+			...jsxA11yPlugin.configs.recommended.rules,
+			"react/react-in-jsx-scope": "off",
+		},
+	},
+
+	/* If uses TypeScript */
+	tseslint.config(
+		{
+			files: ["**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}"],
+			plugins: {
+				import: importPlugin,
+			},
+			settings: {
+				"import/resolver": {
+					typescript: true,
+					node: true,
+				},
+			},
+			rules: {
+				...importPlugin.configs.typescript.rules,
+			},
+		},
+		...tseslint.configs.strictTypeChecked,
+		...tseslint.configs.stylisticTypeChecked,
+		{
+			languageOptions: {
+				parserOptions: {
+					projectService: true,
+					tsconfigRootDir: import.meta.dirname,
+				},
+			},
+		},
+	),
+
 	// Prettier
-	prettierConfig,
+	prettierConfig, // Turns off all rules that are unnecessary or might conflict with Prettier
 
 	// Custom rules
 	{
-		files: ["**/*.{js,cjs,mjs}"],
+		files: ["**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}"],
 		rules: {},
 	},
 
@@ -32,7 +91,7 @@ export default [
 			"**/*.config.cts",
 			"**/*.config.mts",
 
-			// Package manager
+			// Package managers
 			"**/package-lock.json",
 			"**/pnpm-lock.yaml",
 			"**/yarn.lock",
